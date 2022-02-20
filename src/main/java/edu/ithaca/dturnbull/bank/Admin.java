5bc5610c01d22161544b239ec96ab3da312b785f
Annelise Francey <afrancey@ithaca.edu>
2022-02-20 17:42:05 -0500

    added add functions for teller, admin and acc

--- src/main/java/edu/ithaca/dturnbull/bank/Admin.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Admin.java
@@ -1,7 +1,7 @@
 package edu.ithaca.dturnbull.bank;
 
 public class Admin {
-    
+
     public double getAllMoney(){
         return 0.0;
     }
@@ -10,12 +10,22 @@ public class Admin {
         return null;
     }
 
-    public void freeze(Account account){
+
+    /**
+     * @param account
+     * @post freezes account
+     */
+    public void freezeAccount(Account account){
+        account.frozen = true;
 
     }
 
+    /**
+     * @param account
+     * @post unfreezes account
+     */
     public void unfreeze(Account account){
-
+        account.frozen = false;
     }
 
 }
--- src/main/java/edu/ithaca/dturnbull/bank/Bank.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Bank.java
@@ -1,9 +1,38 @@
 package edu.ithaca.dturnbull.bank;
 
-public class Bank {
-    Account[] allAcounts;
-    Teller[] allTellers;
-    Admin[] allAdmin;
+import java.util.ArrayList;
 
-    
+public class Bank {
+    // Account[] allAcounts;
+    // Teller[] allTellers;
+    // Admin[] allAdmin;
+
+    ArrayList<Account> allAccounts = new ArrayList<Account>();
+    ArrayList<Teller> allTellers = new ArrayList<Teller>();
+    ArrayList<Admin> allAdmin = new ArrayList<Admin>();
+
+
+    /**
+     * @param account
+     * @post adds to database of all accounts in system
+     */
+    public void addToAllAccounts(Account account){
+        allAccounts.add(account);
+    }
+
+    /**
+     * @param account
+     * @post adds to database of all tellers in system
+     */
+    public void addToAllTellers(Teller teller){
+        allTellers.add(teller);
+    }
+
+    /**
+     * @param account
+     * @post adds to database of all admin in system
+     */
+    public void addToAllAdmin(Admin admin){
+        allAdmin.add(admin);
+    }
 }
