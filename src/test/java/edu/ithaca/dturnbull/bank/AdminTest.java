ca067180af47e006b15cbcc000047108d079728d
Annelise Francey <afrancey@ithaca.edu>
2022-02-21 10:40:36 -0500

    admin test started

--- src/test/java/edu/ithaca/dturnbull/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/AdminTest.java
@@ -1,5 +1,15 @@
 package edu.ithaca.dturnbull.bank;
 
+import org.junit.jupiter.api.Test;
+
 public class AdminTest {
-    
+    @Test
+    void allMoneyTest(){
+
+        //need ways for accounts to be set up b4 admin can be tested
+        
+        accountSaving account1 = new accountSaving();
+        account1.balance = 
+
+    }
 }
