04683ee7946fa25a69d11d583ae8a54284212d3d
Will Regalado Succop <wregaladosuccop@ithaca.edu>
2022-02-25 11:40:58 -0500

    Bank.checkLogin test

---
+++ src/test/java/edu/ithaca/dturnbull/bank/BankTest.java
@@ -0,0 +1,18 @@
+package edu.ithaca.dturnbull.bank;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class BankTest {
+    
+    @Test
+    public void checkLoginTest(){
+        Customer tCustomer = new Customer("username", "password123", "person@domain.com");
+        
+        assertTrue(Bank.checkLogin("person@domain.com", "password123"));
+        assertFalse(Bank.checkLogin("email", "password1234"));
+    }
+}
