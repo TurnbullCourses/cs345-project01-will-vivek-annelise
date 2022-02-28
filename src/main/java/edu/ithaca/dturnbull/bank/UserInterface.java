6b5beb25a4b11b1e788ff4cc8d323c532f9f4d05
vkumar825 <vkumar825@users.noreply.github.com>
2022-02-28 06:23:50 -0500

    added teller system and modified createAccount

--- src/main/java/edu/ithaca/dturnbull/bank/UserInterface.java
+++ src/main/java/edu/ithaca/dturnbull/bank/UserInterface.java
@@ -4,62 +4,131 @@ import java.util.Scanner;
 
 public class UserInterface {
     public static void main(String[] args) {
-        String usernameIn, passwordIn, emailIn, option;
+        String usernameIn, passwordIn, emailIn, option, choice;
         Customer customer;
+        Teller teller;
+        tellerHuman emp1;
+        double amount;
 
         Scanner scan = new Scanner(System.in);
 
         while (true) {
-
             System.out.println("\nPlease choose one of the options to proceed!");
-            System.out.println("1. Login as Customer");
-            System.out.println("2. Register Customer Account");
+            System.out.println("1. Customer Login");
+            System.out.println("2. Customer Register");
+            System.out.println("3. Teller System");
+            System.out.println("4. Admin System");
             System.out.print("Option: ");
             option = scan.nextLine();
 
-            if (option.equals("1")) {
-                System.out.println("=======================");
+            if (option.equals("1")) { // Customer Login
+                System.out.println("======CUSTOMER LOGIN======");
                 System.out.println("Enter your username:");
                 usernameIn = scan.nextLine();
                 System.out.println("Enter your password:");
                 passwordIn = scan.nextLine();
-                System.out.println("Enter your email:");
+                System.out.println("Enter your valid email:");
                 emailIn = scan.nextLine();
 
+                customer = Bank.getCustomer(emailIn);
 
-                //TODO: Resolve issue with accessing customer inside this while loop. 
-
-                Bank.addLogin(emailIn, passwordIn);
-
-                if (Bank.checkLogin(emailIn, passwordIn)) {
+                if (customer.username.equals(usernameIn)) {
                     customer = Bank.getCustomer(emailIn);
-                    System.out.println("Successfully logged in! Welcome, customer " + customer.username);
+                    System.out.println("Welcome customer " + customer.username);
+                    while (true) {
+                        System.out.println("Welcome, " + customer.username + "! What would you like to do?");
+                        System.out.println("You have " + customer.getAccounts().size() + " accounts.");
+
+                        if (customer.getAccounts().size() == 0) {
+                            System.out.println("Request a Teller to create a Savings or Checking Account for you!");
+                            continue;
+                        }
+
+                        System.out.println("1. Deposit");
+                        System.out.println("2. Withdraw");
+                        System.out.println("3. Transfer");
+                        System.out.println("Type 'e' to exit out.");
+                        System.out.println("Option:");
+                        option = scan.nextLine();
+
+                        if (option.equalsIgnoreCase("e")) {
+                            System.out.println("Goodbye, " + customer.username + "!");
+                            break;
+                        }
+
+                        if (option.equals("1")) {
+                            teller = new tellerHuman();
+
+                            //teller.deposit(account, amount);
+                        }
+
+                    }
                 }
 
                 else {
-                    System.out.println("Wrong email or password!");
+                    System.out.println("Wrong email or password! Please try again!");
+                    break;
                 }
 
             }
 
-            else if (option.equals("2")) {
-                System.out.println("=======================");
+            else if (option.equals("2")) { // Customer Register
+                System.out.println("======CUSTOMER REGISTER======");
                 System.out.println("Create your username:");
                 usernameIn = scan.nextLine();
                 System.out.println("Create your password:");
                 passwordIn = scan.nextLine();
-                System.out.println("Enter your email:");
+                System.out.println("Enter your valid email:");
                 emailIn = scan.nextLine();
 
-                if (Bank.allLogins.containsValue(emailIn)) {
-                    System.out.println("User already exists with that email, please try again!");
-                    continue;
-                }
-
-                System.out.println("Successfully registered!");
+                Bank.addLogin(emailIn, passwordIn);
                 customer = new Customer(usernameIn, passwordIn, emailIn);
                 Bank.allCustomer.add(customer);
+
             }
+
+            else if (option.equals("3")) { // Teller System
+                emp1 = new tellerHuman();
+
+                while (true) {
+                    System.out.println("Welcome employee! Please choose one of the options to proceed.");
+                    System.out.println("Type 'e' to exit.");
+                    System.out.println("1. Create account for a customer");
+                    option = scan.nextLine();
+
+                    if (option.equals("1")) {
+                        System.out.println("Type email of the customer to create account:");
+                        emailIn = scan.nextLine();
+                        System.out.println("Choose which account to create.");
+                        System.out.println("1. Savings");
+                        System.out.println("2. Checking");
+                        choice = scan.nextLine();
+
+                        customer = Bank.getCustomer(emailIn);
+
+                        if (customer.getAccounts().size() == 2){
+                            System.out.println("Cannot create any more account for this user!");
+                            continue;
+                        }
+
+                        if (choice.equals("1")) {
+                            emp1.createAccount(customer, 1);
+                        }
+
+                        if (choice.equals("2")) {
+                            emp1.createAccount(customer, 2);
+                        }
+
+                        else {
+                            System.out.println("Invalid input, please try again!");
+                        }
+                    }
+                }
+            }
+
+            // else if (option.equals("4")) {
+
+            // }
 
             else {
                 System.out.println("Invalid input! Please try again!\n");
--- src/main/java/edu/ithaca/dturnbull/bank/tellerHuman.java
+++ src/main/java/edu/ithaca/dturnbull/bank/tellerHuman.java
@@ -2,16 +2,18 @@ package edu.ithaca.dturnbull.bank;
 
 public class tellerHuman extends Teller{
     
-    public void createAccount(Customer customer){
-
-        //TODO: Find a way to add checking + savings to a customer
+    public void createAccount(Customer customer, int choice){
         
         accountChecking checking = new accountChecking();
         accountSaving saving = new accountSaving();
 
-        customer.accounts.add(checking);
-        customer.accounts.add(saving);
+        if (choice == 1){
+            customer.accounts.add(saving);
+        }
 
+        else if (choice == 2){
+            customer.accounts.add(checking);
+        }
     }
     
     public void closeAccount(Account account){
